bbbccfd0339ffce943d052c554ea47d005d6a669
Pratul Makar <kk953504@gmail.com>
2024-06-28 18:49:52 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,19 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
+import React, { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "sZoom",
+  description: "Digital way to Connect <💕>s",
+  icons: '/icons/logo.svg',
+};
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
